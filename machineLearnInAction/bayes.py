65687a61e16f947b7ec8a85d12213f954a71542b
hongjingjun <hongjingjun@leadeon.cn>
2017-12-15 09:27:40 +0800

    new code

---
+++ machineLearnInAction/bayes.py
@@ -0,0 +1,65 @@
+
+import numpy as np
+
+def loadDataSet():
+    
+    postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'], #[0,0,1,1,1......]
+                   ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                   ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                   ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                   ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                   ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0, 1, 0, 1, 0, 1]  # 1 is abusive, 0 not
+    return postingList, classVec
+
+
+def createVocabList(dataSet):
+    vocabSet = set([])
+    for document in dataSet:
+        vocabSet = vocabSet | set(document)
+    return list(vocabSet)
+
+def setOfWords2Vec(vocabList, inputSet):
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] = 1
+        else:
+            print('the word: %s is not in my vocabulary' % word)
+    return returnVec
+
+def trainNB0(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory) / float(numTrainDocs)
+    p0Num = np.zeros(numWords)
+    p1Num = np.zeros(numWords)
+    p0Denom = 0.0
+    p1Denom = 0.0
+
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+
+    p1Vect = p1Num / p1Denom
+    p0Vect = p0Num / p0Denom
+    return p0Vect, p1Vect, pAbusive
+
+
+if __name__ == '__main__':
+
+    postinList, classVec = loadDataSet()
+    myVocabList = createVocabList(postinList)
+    
+    # print(setOfWords2Vec(myVocabList, postinList[0]))
+    trainMat = []
+    for postinDoc in postinList:
+        trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+    print(trainMat)
+
+    p0V, p1V, pAb = trainNB0(trainMat, classVec)
+    print(p0V,  p1V, pAb)
--- machineLearnInAction/logisticRegression.py
+++ machineLearnInAction/logisticRegression.py
@@ -1,21 +1,117 @@
 
+import numpy as np
+
+def loadDataSet():
+    # mat = np.loadtxt('testSet.txt')
+    # return mat[:, :2], mat[:, -1]
+    dataMat = []
+    labelMat = []
+    f = open('testSet.txt')
+    for line in f.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
+    return dataMat, labelMat
+
 def sigmoid(inX):
-    return 1.0 / (1 + exp(-inX))
+    return 1.0 / (1 + np.exp(-inX))
 
 def gradAscent(dataMatIn, classLabels):
 
-    dataMatrix = mat(dataMatIn)
-    labelMat = mat(classLabels).transpose()
-
-    m, n = shape(dataMatrix)
+    dataMatrix = np.mat(dataMatIn)
+    labelMat = np.mat(classLabels).transpose()
+    
+    m, n = np.shape(dataMatrix)
     alpha = 0.001
 
     maxCycles = 500
-    weights = ones((n, 1))
+    weights = np.ones((n, 1))
 
     for k in range(maxCycles):
         h = sigmoid(dataMatrix * weights)
         error = (labelMat - h)
-        weights = weights + alpha * dataMatrix.transpose() * error
+        step = alpha * dataMatrix.transpose() * error
+        weights = weights + step
     
     return weights
+
+def stocGradAscent0(dataMatrix, classLabels):
+    m,n = np.shape(dataMatrix)
+    alpha = 0.01
+    # n*1的矩阵
+    # 函数ones创建一个全1的数组
+    weights = np.ones(n)   # 初始化长度为n的数组，元素全部为 1
+    for i in range(m):
+        # sum(dataMatrix[i]*weights)为了求 f(x)的值， f(x)=a1*x1+b2*x2+..+nn*xn,此处求出的 h 是一个具体的数值，而不是一个矩阵
+        h = sigmoid(sum(dataMatrix[i]*weights))
+        # print 'dataMatrix[i]===', dataMatrix[i]
+        # 计算真实类别与预测类别之间的差值，然后按照该差值调整回归系数
+        error = classLabels[i] - h
+        # 0.01*(1*1)*(1*n)
+        print weights, "*"*10 , dataMatrix[i], "*"*10 , error
+        weights = weights + alpha * error * dataMatrix[i]
+    return weights
+
+# 随机梯度上升算法（随机化）
+def stocGradAscent1(dataMatrix, classLabels, numIter=150):
+    m,n = np.shape(dataMatrix)
+    weights = np.ones(n)   # 创建与列数相同的矩阵的系数矩阵，所有的元素都是1
+    # 随机梯度, 循环150,观察是否收敛
+    for j in range(numIter):
+        # [0, 1, 2 .. m-1]
+        dataIndex = range(m)
+        for i in range(m):
+            # i和j的不断增大，导致alpha的值不断减少，但是不为0
+            alpha = 4/(1.0+j+i)+0.0001    # alpha 会随着迭代不断减小，但永远不会减小到0，因为后边还有一个常数项0.0001
+            # 随机产生一个 0～len()之间的一个值
+            # random.uniform(x, y) 方法将随机生成下一个实数，它在[x,y]范围内,x是这个范围内的最小值，y是这个范围内的最大值。
+            randIndex = int(np.random.uniform(0,len(dataIndex)))
+            # sum(dataMatrix[i]*weights)为了求 f(x)的值， f(x)=a1*x1+b2*x2+..+nn*xn
+            h = sigmoid(sum(dataMatrix[randIndex]*weights))
+            error = classLabels[randIndex] - h
+            # print weights, '__h=%s' % h, '__'*20, alpha, '__'*20, error, '__'*20, dataMatrix[randIndex]
+            weights = weights + alpha * error * dataMatrix[randIndex]
+            del(dataIndex[randIndex])
+    return weights
+
+def plotBestFit(wei):
+    
+    import matplotlib.pyplot as plt
+
+    weights = wei.getA()
+    dataMat, labelMat = loadDataSet()
+    dataArr = np.array(dataMat)
+
+    n = np.shape(dataArr)[0]
+    xcord1 = []
+    ycord1 = []
+    xcord2 = []
+    ycord2 = []
+
+    for i in range(n):
+        if (int(labelMat[i]) == 1):
+            xcord1.append(dataArr[i, 1]);
+            ycord1.append(dataArr[i, 2])
+        else:
+            xcord2.append(dataArr[i, 1])
+            ycord2.append(dataArr[i, 2])
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=20, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+
+    x = np.arange(-3.0, 3.0, 0.1)
+    y = (-weights[0] - weights[1] * x) / weights[2]
+
+    ax.plot(x, y)
+    plt.xlabel('X1');
+    plt.ylabel('X2')
+    plt.show()
+
+
+if __name__ == '__main__':
+
+    datSet, labels = loadDataSet()
+    weights = gradAscent(datSet, labels)
+    plotBestFit(weights)
